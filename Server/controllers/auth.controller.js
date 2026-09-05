dcfc100a50b3f0bb79230f1ba07f8bc6f61e58cf
Karan Sahu <161184262+Iamkaran00@users.noreply.github.com>
2026-09-05 08:27:47 +0530

    Change SameSite cookie attribute to 'none'

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -18,7 +18,7 @@ const generateToken = user => {
 const cookieOptions = {
   httpOnly : true , 
   secure : process.env.NODE_ENV === 'production' , 
-  sameSite : "lax" , 
+  sameSite : "none" , 
   maxAge : 7*24*60*60*1000,
 };
  export const registerUser = async (req ,res) => {
